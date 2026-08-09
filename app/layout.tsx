2b481c4fbb2812fdbf8e3ddfab58a302c89668b0
958813652 <a958813653@958813652deMacBook-Air.local>
2026-08-08 17:18:16 -0700

    Animate handwriting guidance

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import "./adventure.css";
+import "./writing-auto.css";
 
 const nunito = Nunito({
   variable: "--font-nunito",
